ed7cfcb298e72ee4309aeb8ec7c189e0f554b59f
Mark J. Reed <markjreed@gmail.com>
2024-01-13 10:54:50 -0500

    feat: adopt dict approach

--- langtons-loops/langtons-loops.py
+++ langtons-loops/langtons-loops.py
@@ -1,12 +1,13 @@
 import argh
+import math
 import rules
 import sys
+import time
 import tkinter
-from tkinter import messagebox
 
-cell_size     = 8
-canvas_width  = 640
-canvas_height = 480
+cell_size     = 4
+canvas_width  = 320
+canvas_height = 240
 
 state_colors = [ 'black',  'blue',   'red',   'green',
                  'yellow', 'magenta','white', 'cyan' ]
@@ -37,20 +38,25 @@ tk.protocol('WM_DELETE_WINDOW', on_close)
 def main():
     cell_width  = canvas_width  // cell_size
     cell_height = canvas_height // cell_size 
-    loop_x = 32
-    loop_y = 25
+    corner_x = -32
+    corner_y = -25
     items = []
-    states = [ [0 for j in range(cell_width)] for i in range(cell_height) ]
-    for i, row in enumerate(loop):
-        for j, state in enumerate(row):
-            states[i+loop_y][j+loop_x] = state
+    states = {}
+    for y, row in enumerate(loop):
+        items.append([])
+        for x, state in enumerate(row):
+            states[f'{x},{y}'] = state
 
+    start = time.time()
+    generation = 0
     c = tkinter.Canvas(tk, width=canvas_width, height=canvas_height, background='black')
     items = []
     for i in range(cell_height):
+        y = i + corner_y
         items.append([])
         for j in range(cell_width):
-            state = states[i][j]
+            x = i + corner_x
+            state = states.get(f'{x},{y}',0)
             item = c.create_rectangle(j*cell_size, i*cell_size,
                                       (j+1)*cell_size-1, (i+1)*cell_size-1,
                                       outline=state_colors[state],
@@ -66,114 +72,66 @@ def main():
 
     c.pack()
 
-    while not quitting:
-        buffer = [[0 for i in row] for row in states]
-        for i in range(cell_height):
-            for j in range(cell_width):
-                self = states[i][j]
-                n = states[i-1][j] if i else states[cell_height-1][j]
-                e = states[i][j+1] if j < cell_width-1 else states[i][0]
-                s = states[i+1][j] if i < cell_height-1 else states[0][j]
-                w = states[i][j-1] if j else states[i][cell_width-1]
-                key = f'{self}{n}{e}{s}{w}'
-                if key in transition_table:
-                     buffer[i][j] = transition_table[key]
-                else:
-                    buffer[i][j] = self
+    done = False
+    while not quitting and not done:
+        buffer = {}
+        to_scan = set()
+        done = True
+
+        for coords, state in states.items():
+            x, y = [int(v) for v in coords.split(',')]
+            for d in range(-1,2):
+                to_scan.add(f'{x+d},{y}')
+                to_scan.add(f'{x},{y+d}')
+
+        min_x = min_y = math.inf
+        max_x = max_y = -math.inf
+        for coords in to_scan:
+            self = states.get(coords, 0)
+            x, y = [int(v) for v in coords.split(',')]
+            n = states.get(f'{x},{y-1}',0)
+            e = states.get(f'{x+1},{y}',0)
+            s = states.get(f'{x},{y+1}',0)
+            w = states.get(f'{x-1},{y}',0)
+            key = f'{self}{n}{e}{s}{w}'
+            if key in transition_table:
+                new = transition_table[key]
+                if (new != self and 
+                    y >=corner_y and y < corner_y + cell_height and
+                    x >=corner_x and x < corner_x + cell_width):
+                    done = False
+            else:
+                new = self
+            if new:
+                if x < min_x: min_x = x 
+                if x > max_x: max_x = x 
+                if y < min_y: min_y = y 
+                if y > max_y: max_y = y 
+                buffer[coords] = new
 
         states = buffer
         for i in range(cell_height):
-            if quitting:
-                break
+            y = i + corner_y
             for j in range(cell_width):
-                state = states[i][j]
+                x = j + corner_x
+                state = states.get(f'{x},{y}', 0)
                 color = state_colors[state]
-                if quitting:
-                    break
                 c.itemconfigure(items[i][j], outline=color, fill=color)
 
-        if not quitting:
-            tk.update()
+        generation += 1
+        tk.update()
+
+
+    end = time.time()
+    print(f'Finished in {end - start} seconds after {generation} generations')
+    print(f'Final tally: {len(states)} cells with nonzero states')
+    print(f'Bounding box: {min_x},{min_y} to {max_x},{max_y}')
+
+    while not quitting:
+        tk.update()
 
     tk.destroy()
 
 if __name__ == '__main__':
     argh.dispatch_command(main)
 
-
-#    sub set_state(ubyte x, ubyte y, ubyte state) {
-#        gfx2.fillrect(x as uword * cell_size, y as uword * cell_size,
-#                      cell_size, cell_size, state_colors[state])
-#    }
-#
-#    sub get_state(ubyte x, ubyte y) -> ubyte {
-#        uword pixel_x = x as uword * cell_size + cell_size>>1
-#        uword pixel_y = y as uword * cell_size + cell_size>>1
-#        return color_states[gfx2.pget(pixel_x, pixel_y)]
-#    }
-#
-#    sub set_offscreen_state(ubyte x, ubyte y, ubyte state) {
-#        @(new_grid + y as uword * grid_width as uword + x as uword) = state
-#    }
-#
-#    sub get_offscreen_state(ubyte x, ubyte y) -> ubyte {
-#        return @(new_grid + y as uword * grid_width as uword + x as uword)
-#    }
-#
-#    sub copy_grid() {
-#        ubyte x, y
-#        for y in 0 to grid_height - 1 {
-#            for x in 0 to grid_width - 1 {
-#                set_state(x, y, get_offscreen_state(x, y))
-#            }
-#        }
-#    }
-#
-#    sub start() {
-#        ubyte x, y, i, key
-#        ubyte[6] state_string
-#        uword rule
-#        ubyte new_state
-#        bool found
-#
-#        gfx2.screen_mode(1)     ; 320 x 240 x 8bpp
-#        gfx2.clear_screen(0)
-#        i = 0
-#        for y in 47 to 56 {
-#            for x in 2 to 17 {
-#                set_state(x, y, loop[i])
-#                i += 1
-#            }
-#        }
-#        state_string[5] = 0
-#        do {
-#            for y in 1 to grid_height - 2 {
-#                emudbg.console_value1(y)
-#                for x in 1 to grid_width - 2 {
-#                   state_string[0] = '0' + get_state(x, y)
-#                   state_string[1] = '0' + get_state(x, y-1)
-#                   state_string[2] = '0' + get_state(x+1, y)
-#                   state_string[3] = '0' + get_state(x, y+1)
-#                   state_string[4] = '0' + get_state(x-1, y)
-#                   found = false
-#                   for i in 0 to len(langton.rules) {
-#                       rule = &langton.rules[i]
-#                       new_state = rule[5] - '0'
-#                       rule[5] = 0
-#                       if string.compare(state_string, rule) == 0 {
-#                           set_offscreen_state(x, y, new_state)
-#                           found = true
-#                       }
-#                       rule[5] = new_state + '0'
-#                       if found { break }
-#                   }
-#                   if not found {
-#                       set_offscreen_state(x, y, get_state(x, y))
-#                   }
-#                }
-#            }   
-#            copy_grid()
-#            key = cbm.GETIN()
-#        } until key
-#    }
-#}
